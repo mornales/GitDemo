ad7c8f19fe67b00038e280578ff696284d299527
mornales <maricarornales28@gmail.com>
2021-01-16 18:22:03 +0800

    first commit

---
+++ src/test/java/qaclickacademy/appiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class appiumTest {
+
+	@Test
+	public void NativeAppAndroid() {
+		System.out.println("NativeAppAndroid");
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+
+	}
+}
---
+++ src/test/java/qaclickacademy/restAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class restAPITest {
+
+	@Test
+	public void posJira() {
+		System.out.println("posJira");
+
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+
+	}
+
+}
